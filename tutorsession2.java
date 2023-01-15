baaaf7262f74d76f0be66d3a5e1a5ea6dd5522b6
Caitlin Anchor <anchorcaitlin03@gmail.com>
2023-01-15 17:39:04 +1030

    add

---
+++ tutorsession2.java
@@ -0,0 +1,10 @@
+package caitlinanchor;
+import java.util.Scanner;
+
+public class tutorsession2 {
+    public static void main(String[] argument){
+        Scanner scan = new Scanner(System.in);
+        int status = 1;
+        tutorsession tutorlist[] = null;  
+    }
+}
