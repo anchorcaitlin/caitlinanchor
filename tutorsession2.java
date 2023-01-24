7c51072f62466dc6971634e9972cc98d223ef4dc
caitlinanchor <109566913+caitlinanchor@users.noreply.github.com>
2023-01-24 16:42:00 +1030

    Delete tutorsession2.java

--- tutorsession2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class tutorsession2 {
-    public static void main(String[] argument){
-        Scanner scan = new Scanner(System.in);
-        int status = 1;
-        tutorsession tutorlist[] = null;  
-    }
-}
